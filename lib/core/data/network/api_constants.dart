436b5691b2f935f0ea1305d5ef8b3b6e522cd27f
ahmeed2008 <133704884+ahmeed2008@users.noreply.github.com>
2025-10-27 05:51:05 +0300

    Update api_constants.dart
    add api key

--- lib/core/data/network/api_constants.dart
+++ lib/core/data/network/api_constants.dart
@@ -1,7 +1,7 @@
 import 'package:movies_app/tv_shows/domain/usecases/get_season_details_usecase.dart';
 
 class ApiConstants {
-  static const String apiKey = '';
+  static const String apiKey = '432ed4524712cc9cad8766856b23f4d6';
   static const String baseUrl = 'https://api.themoviedb.org/3';
 
   static const String baseBackdropUrl = 'https://image.tmdb.org/t/p/w1280';
